39bf3b645bd97884ca782dd96ea6793a06385ed2
XodTuza <gordie2u@gmail.com>
2020-07-12 12:51:37 +0100

    fixes

--- config/database.js
+++ config/database.js
@@ -6,9 +6,7 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
-// config/database.js
-const Url = require('url-parse')
-const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
+
 
 module.exports = {
   /*
@@ -52,15 +50,17 @@ module.exports = {
   | npm i --save mysql
   |
   */
-// config/database.js
-
-connection: {
-  host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.us-cdbr-east-02.cleardb.com),
-  port: Env.get('DB_PORT', ''),
-  user: Env.get('DB_USER', CLEARDB_DATABASE_URL.bc022ba5586e33),
-  password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.0d4b3096),
-  database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.heroku_3b05248e8009552.substr(1))
-},
+  mysql: {
+    client: 'mysql',
+    connection: {
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
+    },
+    debug: Env.get('DB_DEBUG', false)
+  },
 
   /*
   |--------------------------------------------------------------------------
