55ec8f2188b9cc672c0493f26a3c724cd878452c
XodTuza <gordie2u@gmail.com>
2020-07-12 13:00:41 +0100

    fix

--- config/database.js
+++ config/database.js
@@ -6,8 +6,6 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
-
-
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -53,11 +51,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'us-cdbr-east-02.cleardb.com'),
+      host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'bc022ba5586e33'),
-      password: Env.get('DB_PASSWORD', '0d4b3096'),
-      database: Env.get('DB_DATABASE', 'heroku_3b05248e8009552')
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
     },
     debug: Env.get('DB_DEBUG', false)
   },
