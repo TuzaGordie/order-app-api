562de8421f378156cf91ed49f392f542fd6c5b11
XodTuza <gordie2u@gmail.com>
2020-07-17 12:16:39 +0100

    log in notification

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -9,11 +9,41 @@ class UserController {
   async login({ request, auth }) {
       const { email, password } = request.all();
       const token = await auth.attempt(email, password);
+
+    //send notification email
+    //Step 1 
+    const auth =  {
+      auth: {
+        api_key: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN
+      }
+    };
+
+    //Step 2 
+    let transporter = nodemailer.createTransport( nodemailMailgun(auth) );
+
+    //Step 3 
+    const mailOptions = {
+      from: 'Gordie<sophisticateddev@gmail.com>',
+      to: request.body.email,
+      subject: 'Auth',
+      text: 'You have successfully logged in to your Order app account'
+    }
+
+    // Step 4 
+    transporter.sendMail(mailOptions, function(err, data) {
+      if(err) {
+        console.log('Error: ', err);
+      } else {
+        console.log('Email Sent!!!');
+      }
+    });
+
       return token;
+      
   }
 
   async register({ request }) {
-    console.log('hey ninja!')
     //Create User
     const { email, password } = request.all();
     await User.create({
